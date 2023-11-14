731a1efbd68d018dc3397ef035d9361772bec2cb
des0912 <141937626+des0912@users.noreply.github.com>
2023-11-14 15:05:23 +0800

    faker
    Records_app

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+define('ROOT_URL', 'http://localhost/Records_app/');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'Recordsapp');
+?>
---
+++ script/employee_delete.php
@@ -0,0 +1,14 @@
+<<?php
+
+require('config/config.php');
+require('config/db.php');
+
+$id = $_GET['id'];
+$query = "DELETE FROM employee WHERE id=" . $id;
+
+if(mysqli_query($conn, $query)){
+}else{
+    echo 'ERROR: '.mysqli_error($conn);
+}
+
+header("Location:index.php");
